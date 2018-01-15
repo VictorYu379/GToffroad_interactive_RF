6a4eecd987ee68dd89de642bd6264d41a15abcf5
Victor Yu <victoryu379@gmail.com>
2018-01-14 20:56:08 -0500

    adding server side and client side arduino code

---
+++ sketch_jan11_client/sketch_jan11_client.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+
+const byte HC12RxdPin = 2;                  // Recieve Pin on HC12
+const byte HC12TxdPin = 12;                  // Transmit Pin on HC12
+int counter = 97;
+
+SoftwareSerial HC12(HC12TxdPin,HC12RxdPin); // Create Software Serial Port
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  HC12.begin(9600);
+//  Serial.println("begiy begin");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (HC12.available()) {
+    Serial.print((char) HC12.read());
+  }
+
+  if (Serial.available()) {
+    char a = Serial.read();
+    Serial.print("Write: ");
+    Serial.println(a);
+    HC12.write(a);   
+  }
+}
+
+
